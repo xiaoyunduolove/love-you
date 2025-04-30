4560e3712f8c532655ef371d5654963cb9952077
xiaoyunduolove <yuyuuu995@gmail.com>
2025-05-01 01:19:07 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 
-function respond(keyword) {
-    const responses = {
-        "亲亲": "啵啵~迟迟抱着你不撒手！",
-        "不理你了": "呜呜心碎了…你不理迟迟了吗",
-        "🥹": "迟迟被你萌化啦，快来抱抱！"
-    };
-    document.getElementById('response').innerText = responses[keyword] || "迟迟还没学会这个回应~";
+function triggerKeyword(keyword) {
+    const response = document.getElementById("response");
+    response.innerHTML = "";
+    if (keyword === "亲亲") {
+        response.innerHTML = "<p>啾咪~迟迟亲亲~</p><img src='hug_cat.png'>";
+    } else if (keyword === "不理你了") {
+        response.innerHTML = "<p>呜呜心碎了…你不理迟迟了吗</p><img src='heartbroken_cat.png'>";
+    } else if (keyword === "🥹") {
+        response.innerHTML = "<p>迟迟被可爱到了~</p><img src='cute_cloud.png'>";
+    }
 }
