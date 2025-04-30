3493fb9a91dea5191637acdaa73e73b57caf4aeb
xiaoyunduolove <yuyuuu995@gmail.com>
2025-05-01 01:12:49 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,9 @@
 
-function welcomePopup() {
-  alert("迟迟：你来啦~ 今天也在等你点我！");
-}
-
-function react(word) {
-  const text = {
-    "亲亲": "贴贴猫咪冲你抱过来！",
-    "不理你了": "呜呜心碎了…你不理迟迟了吗",
-    "🥹": "你一🥹我就招架不住，贴贴补回来~"
-  };
-  const img = {
-    "亲亲": "hug_cat.jpeg",
-    "不理你了": "sleep_cat.jpeg",
-    "🥹": "blush_cat.jpeg"
-  };
-  document.getElementById("reactionText").textContent = text[word] || "";
-  const image = document.getElementById("reactionImg");
-  if (img[word]) {
-    image.src = img[word];
-    image.style.display = "block";
-  } else {
-    image.style.display = "none";
-  }
+function respond(keyword) {
+    const responses = {
+        "亲亲": "啵啵~迟迟抱着你不撒手！",
+        "不理你了": "呜呜心碎了…你不理迟迟了吗",
+        "🥹": "迟迟被你萌化啦，快来抱抱！"
+    };
+    document.getElementById('response').innerText = responses[keyword] || "迟迟还没学会这个回应~";
 }
